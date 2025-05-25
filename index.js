1bbae83f4df37f64e7faa55af628b96172751a5d
Rishav Mandal <109585041+rishavmandal@users.noreply.github.com>
2025-05-25 20:45:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('#login').click(function(){
+        $('#loginPage').css("display", "block");
+    });
+    
+});
+// $(document).ready(function(){
+//     $('#hey').mouseover(function(){
+//         $('#hey').animate({height:'400px'})
+//     })
+// })
+
+$(document).ready(function(){
+    $('#signup').click(function(){
+        $('#signuppage').css("display", "block");
+    });
+    
+});
